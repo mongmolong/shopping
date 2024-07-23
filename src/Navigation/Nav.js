3f95aa3e22b63fbc1488a787b22148cb527a11e4
mongmolong <nisatang@naver.com>
2024-07-23 17:21:03 +0900

    second

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,30 +1,49 @@
-import { GoHeart } from "react-icons/go";
-import { LuShoppingCart } from "react-icons/lu";
-import { FiUserPlus } from "react-icons/fi";
+import { RiHeart3Line  } from "react-icons/ri";
+import { RiHeartAdd2Line } from "react-icons/ri";
+
+import { TbShoppingCart } from "react-icons/tb";
+import { TbShoppingCartCheck } from "react-icons/tb";
+import { GoPerson, GoPersonAdd  } from "react-icons/go";
+
 import { useState } from "react";
 
 import "./Nav.css";
 
-function Nav() {
+function Nav({ query, handleputChange }) {
+
+  const [isHearted, setIsHearted] = useState(false); // 초기 상태는 false로 설정
+  const [isCart, setIsCart] = useState(false); 
+  const [isPerson, setIsPerson] = useState(false); 
+
+  const handleClick = () => {
+    setIsHearted(!isHearted); // 상태를 반전시킴
+  };
+  const handleClick2 = () => {
+    setIsCart(!isCart);
+  };
+  const handleClick3 = () => {
+    setIsPerson(!isPerson); 
+  };
 
   return (
     <nav>
       <div className="nav-container">
-        <input className="search-input" type="text" placeholder="Enter your search shoes." />
+        <input
+          query={query}
+          onChange={handleputChange}
+          className="search-input" type="text" placeholder="Enter your search shoes." />
       </div>
       <div className="profile-container">
-        <a href="#">
-          <GoHeart  className="nav-icons" />
+        <a href="#" onClick={handleClick}>
+          {isHearted ? <RiHeartAdd2Line className="nav-icons" /> : <RiHeart3Line  className="nav-icons" />}
         </a>
-        <a href="#">
-          <LuShoppingCart className="nav-icons"/>
+        <a href="#" onClick={handleClick2}>
+          {isCart ? <TbShoppingCartCheck className="nav-icons" /> : <TbShoppingCart className="nav-icons" />}
         </a>
-        <a href="#">
-          <FiUserPlus  className="nav-icons"/>
+        <a href="#" onClick={handleClick3}>
+          {isPerson ? <GoPersonAdd className="nav-icons"/> : <GoPerson className="nav-icons" /> }
         </a>
       </div>
-
-
     </nav>
   )
 }
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,17 +1,26 @@
-import { BsBagHeartFill } from "react-icons/bs";
+import { useState } from "react";
+import { BsBagHeart, BsBagHeartFill} from "react-icons/bs";
 
-function Card({img, title, star, reviews, newPrice, prevPrice}) {
-  return (
+function Card({ img, title, star, reviews, newPrice, prevPrice }) {
+  const [ isKeep, setIsKeep] = useState(false);
+
+  const keepClick = () => {
+    setIsKeep(!isKeep);
+  }
+
+return (
     <>
       <section className="card ">
-        <img 
-        src={img}
-        alt={title}
-        className="card-img" />
+        <div className="card-img-top">
+          <img
+          src={img}
+          alt={title}
+          className="card-img" />
+        </div>
 
         <div >
           <h3 className="card-title">{title}</h3>
-          <section className="card-reviews">         
+          <section className="card-reviews">
             {star} {star} {star} {star}
             <span className="total-reviews">{reviews}</span>
           </section>
@@ -20,12 +29,12 @@ function Card({img, title, star, reviews, newPrice, prevPrice}) {
             <div className="price">
               <del>{prevPrice}</del>&nbsp;{newPrice}
             </div>
-            <div className="bag">
-              <BsBagHeartFill className="bag-icon" />
+            <div className="bag" onClick={keepClick}>
+              {isKeep? <BsBagHeartFill className="bag-icon" /> : <BsBagHeart  className="bag-icon" />}
             </div>
           </section>
         </div>
-      </section>
+      </section >
     </>
   )
 }
